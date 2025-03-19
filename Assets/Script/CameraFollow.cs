3d873e30ff6cfb0b2a043fcedb4d5111874da701
louie0523 <151519353+louie0523@users.noreply.github.com>
2025-03-19 17:36:35 +0900

    밀리는 함정 힘 제대로 추가

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -7,7 +7,7 @@ public class CameraFollow : MonoBehaviour
     public Transform target;
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
-    private void LateUpdate()
+    private void Update()
     {
         Vector3 desirdPos = target.position + offset;
         Vector3 SmoothPos = Vector3.Lerp(this.transform.position, desirdPos, smoothSpeed);
--- Assets/Script/Trap.cs
+++ Assets/Script/Trap.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Trap : MonoBehaviour
 {
     Animator animator;
+    public float Power = -15f;
     public float Waittime = 10f;
 
     private void Start()
@@ -21,5 +22,12 @@ public class Trap : MonoBehaviour
         yield return new WaitForSeconds(Waittime);
         }
 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player")) {
+            collision.rigidbody.AddForce(100 * Power, 0, 0);
+        }
     }
 }
